2bc35726170997ed42a38e3c43426fd4ca56ae5a
Yury Reshetnikov <reshu@mail.ru>
2024-02-28 14:34:45 +0500

    _

--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,15 @@ using std::flush;
 namespace fs = boost::filesystem;
 
 int main(int argc, const char **argv) {
-	cout << "started" << endl << flush;
 	fs::path folder = fs::current_path();
-	cout << "folder:" << folder.c_str() << endl << flush;
-	fs::path filename = folder.filename();
-	cout << "filename:" << filename.c_str() << endl << flush;
-	// for(auto ptr = folder.begin(), end = folder.end(); ptr != end; ++ptr)
-	// 	cout << "\t* " << ptr->c_str() << endl << flush;
-	// for(auto &&ptr : folder)
-	// 	cout << "\t+ " << ptr.c_str() << endl << flush;
-	// fs::path rbegin = *folder.rbegin();
-	// cout << "rbegin:" << rbegin.c_str() << endl << flush;
-	// fs::path rend = *folder.rend();
-	// cout << "rend:" << rend.c_str() << endl << flush;
-	// for(auto ptr = folder.rbegin(), end = folder.rend(); ptr != end; ++ptr)
-	// 	cout << "\t* " << ptr->c_str() << endl << flush;
+	cout << "folder:" << folder << endl;
+	cout << "filename:" << folder.filename() << endl << flush;
+	for(auto ptr = folder.begin(), end = folder.end(); ptr != end; ++ptr)
+		cout << "\t* " << ptr->c_str() << endl << flush;
+	for(auto &&ptr : folder)
+		cout << "\t+ " << ptr.c_str() << endl << flush;
+	for(auto ptr = folder.rbegin(), end = folder.rend(); ptr != end; ++ptr)
+		cout << "\t* " << ptr->c_str() << endl << flush;
 	return 0;
 }
 
