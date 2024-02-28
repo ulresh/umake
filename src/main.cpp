1a2aee5af4434cf9ad4e69efd50bea5faa07b3b2
Yury Reshetnikov <reshu@mail.ru>
2024-02-29 03:47:03 +0500

    _

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,18 @@
+#include <iostream>
+#include <boost/filesystem.hpp>
+
+using std::cout;
+using std::endl;
+using std::flush;
+namespace fs = boost::filesystem;
+
 int main(int argc, const char **argv) {
+	fs::path folder = fs::current_path();
+	cout << "folder:" << folder << endl;
+	cout << "filename:" << folder.filename() << endl;
+	cout << "parent:" << folder.parent_path() << endl;
+	for(auto ptr = folder.rbegin(), end = folder.rend(); ptr != end; ++ptr)
+		cout << "\t* " << *ptr << endl;
 	return 0;
 }
 
