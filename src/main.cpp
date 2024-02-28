4b793135b9ba95d9a1ba03bae7e240c1f46b51db
Yury Reshetnikov <reshu@mail.ru>
2024-02-28 15:13:09 +0500

    _

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,42 @@
 #include <boost/filesystem.hpp>
 
 using std::cout;
+using std::cerr;
 using std::endl;
 using std::flush;
 namespace fs = boost::filesystem;
 
+bool has_git_folder(fs::path folder) {
+	folder /= ".git";
+	return fs::exists(folder);
+}
+
 int main(int argc, const char **argv) {
 	fs::path folder = fs::current_path();
 	cout << "folder:" << folder << endl;
+	if(has_git_folder(folder)) {
+		cerr << "Невозможен запуск в корне проекта" << endl;
+		return 1;
+	}
 	cout << "filename:" << folder.filename() << endl;
 	cout << "parent:" << folder.parent_path() << endl;
-	for(auto ptr = folder.rbegin(), end = folder.rend(); ptr != end; ++ptr)
-		cout << "\t* " << *ptr << endl;
+	fs::path up = folder.filename();
+	bool in_src = up == "src";
+	for(;;) {
+		folder = folder.parent_path();
+		if(folder.empty()) {
+			cerr << "Не получилось найти папку .git выше текущей папки"
+				 << endl;
+			return 1;
+		}
+		if(has_git_folder(folder)) break;
+		fs::path n = up;
+		up = folder.filename();
+		in_src = up == "src";
+		up /= n;
+	}
+	cout << "project:" << folder << " source:" << up
+		 << " in_src:" << in_src << endl;
 	return 0;
 }
 
