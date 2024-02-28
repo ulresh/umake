5a18c5d5e960fb76e20c4b6be8d361e86843dcb5
Yury Reshetnikov <reshu@mail.ru>
2024-02-28 14:36:10 +0500

    _

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,9 @@ namespace fs = boost::filesystem;
 int main(int argc, const char **argv) {
 	fs::path folder = fs::current_path();
 	cout << "folder:" << folder << endl;
-	cout << "filename:" << folder.filename() << endl << flush;
-	for(auto ptr = folder.begin(), end = folder.end(); ptr != end; ++ptr)
-		cout << "\t* " << ptr->c_str() << endl << flush;
-	for(auto &&ptr : folder)
-		cout << "\t+ " << ptr.c_str() << endl << flush;
+	cout << "filename:" << folder.filename() << endl;
 	for(auto ptr = folder.rbegin(), end = folder.rend(); ptr != end; ++ptr)
-		cout << "\t* " << ptr->c_str() << endl << flush;
+		cout << "\t* " << ptr->c_str() << endl;
 	return 0;
 }
 
