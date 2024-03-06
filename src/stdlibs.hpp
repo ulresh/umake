f79dbde492db021eefa4f906659a33048a7068cb
Yury Reshetnikov <reshu@mail.ru>
2024-03-06 11:34:01 +0500

    _

--- src/stdlibs.hpp
+++ src/stdlibs.hpp
@@ -9,6 +9,8 @@
 #include <boost/asio.hpp>
 #include <boost/bind/bind.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/iostreams/tee.hpp>
+#include <boost/iostreams/stream.hpp>
 
 using std::cout;
 using std::cerr;
@@ -29,6 +31,10 @@ using boost::posix_time::seconds;
 using boost::posix_time::minutes;
 using boost::posix_time::time_duration;
 using boost::posix_time::from_time_t;
+
+extern std::ofstream ulog;
+typedef boost::iostreams::tee_device<std::ostream,std::ostream> TeeDevice;
+extern boost::iostreams::stream<TeeDevice> uout;
 
 /*
  * Local Variables:
