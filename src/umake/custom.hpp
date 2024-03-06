1729455ca0e32537635d817857458b03446c5b42
Yury Reshetnikov <reshu@mail.ru>
2024-03-06 11:21:23 +0500

    _

--- src/umake/custom.hpp
+++ src/umake/custom.hpp
@@ -4,13 +4,18 @@
 #include <string>
 
 struct Custom {
+	void add_system_include_path(const std::string &s) {
+		system_include_pathes.push_back(s); }
+	void add_sysinc(const std::string &s) {
+		system_include_pathes.push_back(s); }
 	void add_include_path(const std::string &s) {
 		include_pathes.push_back(s); }
 	void add_library(const std::string &s) {
 		libraries.push_back(s); }
 	void add_library_file(const std::string &s) {
 		library_files.push_back(s); }
-	std::list<std::string> include_pathes, libraries, library_files;
+	std::list<std::string> system_include_pathes,
+		include_pathes, libraries, library_files;
 };
 
 /*
