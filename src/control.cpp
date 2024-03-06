a4b2d582bf89fa304dca050eb5df21d346d62f55
Yury Reshetnikov <reshu@mail.ru>
2024-03-06 11:32:58 +0500

    _

--- src/control.cpp
+++ src/control.cpp
@@ -1,14 +1,14 @@
 #include "control.hpp"
 
 void Control::start(const std::string &source,
-			std::time_t source_mtime, const std::string &dependencies,
+			std::time_t object_mtime, const std::string &dependencies,
 			const std::string &cmd, std::list<std::string> args) {
 	cout << cmd;
 	for(auto &&a : args) cout << ' ' << a;
 	cout << endl;
 	Compiler *p;
 	std::unique_ptr<Compiler>
-		h(p = new Compiler(*this, source, source_mtime, dependencies,
+		h(p = new Compiler(*this, source, object_mtime, dependencies,
 						   cmd, args));
 	compilers[p->child.id()].reset(h.release());
 	p->async_start_pipes();
