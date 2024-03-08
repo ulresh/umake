e00b972b00063e9b77abcb90f32d7bbff3747b5b
Yury Reshetnikov <reshu@mail.ru>
2024-03-08 13:53:31 +0500

    _

--- src0/main.cpp
+++ src0/main.cpp
@@ -83,7 +83,8 @@ void load_custom_file(Custom &custom, RootFolder &root_folder,
 			auto inc_env = env["UMAKE_CUSTOM_INCLUDE_PATH"];
 			if(inc_env.empty()) {
 #ifdef UMAKE_DEFAULT_CUSTOM_INCLUDE_PATH
-#define SM(x) #x
+#define SM2(x) #x
+#define SM(x) SM2(x)
 				inc = "-I" SM(UMAKE_DEFAULT_CUSTOM_INCLUDE_PATH);
 #else
 				inc = "-I../src0";
